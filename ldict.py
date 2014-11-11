ca8b2d6ba261630e20fbdb41f2286458c0d16d57
guozengxin <guozengxin@outlook.com>
2014-11-11 16:07:47 +0800

    add code in ldict.py, it can handle up arrow and down arrow event

--- ldict.py
+++ ldict.py
@@ -7,32 +7,34 @@ import getopt
 from ldutil import htmlfetcher
 from ldutil.htmlparser import HtmlXPathParser
 from ldutil import colorprint
+import tty
+import termios
 
 outputEncoding = 'gb18030'
-historyFile = os.path.join(os.path.expanduser('~'), '.iciba_history')
+historyFile = os.path.join(os.path.expanduser('~'), '.ldict_history')
 historyList = []
 
 
-class _GetchUnix:
-
-    def __init__(self):
-        pass
-
-    def __call__(self):
-        import sys
-        import tty
-        import termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+class _Getch:
+    def __call__(self, length=1):
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(sys.stdin.fileno())
+                ch = sys.stdin.read(length)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            return ch
 
 
-getch = _GetchUnix()
+getch = _Getch()
+
+
+def getInput():
+    inKey = getch()
+    if ord(inKey) == 27:
+        inKey += getch(2)
+    return inKey
 
 
 def readHistory():
@@ -59,25 +61,52 @@ def usage():
     print '''iciba.py [-d(--daemon)|-h(--help)|word]'''
 
 
+def removeShow(chars):
+    for i in range(0, len(chars)):
+        sys.stdout.write('\b \b')
+
+
 def myinput(flag='> '):
     chars = []
     sys.stdout.write(flag)
+    nowIndex = len(historyList)
+    hislen = nowIndex
     while True:
-        newChar = getch()
-        if newChar in '\r\n':
-            print ''
-            break
-        elif newChar == '\b':
-            if chars:
-                del chars[-1]
-                sys.stdout.write('\b \b')
-        elif newChar in '\03\04':
-            sys.exit(-1)
-        elif ord(newChar) == 72:
-            pass
-        else:
-            sys.stdout.write(newChar)
-            chars.append(newChar)
+        newChar = getInput()
+        newLen = len(newChar)
+        if newLen == 1:
+            if str(newChar) in '\r\n':
+                print ''
+                break
+            elif newChar == '\b':
+                if chars:
+                    del chars[-1]
+                    sys.stdout.write('\b \b')
+            elif str(newChar) in '\03\04':
+                sys.exit(-1)
+            elif ord(str(newChar)) == 72:
+                pass
+            else:
+                sys.stdout.write(newChar)
+                chars.append(newChar)
+        elif newLen == 3:
+            if newChar == '\x1b[A':
+                if nowIndex > 0:
+                    if nowIndex == hislen:
+                        removeShow(chars)
+                    elif nowIndex > 0:
+                        removeShow(historyList[nowIndex])
+                    sys.stdout.write(historyList[nowIndex - 1])
+                    nowIndex -= 1
+            elif newChar == '\x1b[B':
+                if nowIndex < len(historyList):
+                    removeShow(historyList[nowIndex])
+                    if nowIndex < len(historyList) - 1:
+                        sys.stdout.write(historyList[nowIndex + 1])
+                    elif nowIndex == len(historyList) - 1:
+                        sys.stdout.write(''.join(chars))
+                    nowIndex += 1
+
     return ''.join(chars)
 
 
@@ -191,22 +220,16 @@ def translate(word):
 
 def main():
     config, args = parseOpt(sys.argv[1:])
-    if config['daemon']:
-        readHistory()
-        while True:
-            # line = raw_input('> ')
-            line = myinput()
-            word = line.strip()
-            translate(word)
-            sys.stdout.flush()
-            sys.stderr.flush()
-    else:
-        if len(args) == 0:
-            usage()
-            sys.exit()
-        word = args[0]
+    readHistory()
+    while True:
+        # line = raw_input('> ')
+        line = myinput()
+        word = line.strip()
+        if len(word) == 0:
+            continue
         translate(word)
-
+        sys.stdout.flush()
+        sys.stderr.flush()
 
 if __name__ == '__main__':
     main()
